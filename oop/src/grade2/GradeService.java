a4e478e3805bbbc2663fe069c653a32c3216377b
hb2020 <hb2020@hb2020-PC>
2016-06-21 17:53:38 +0900

    17:53

---
+++ oop/src/grade2/GradeService.java
@@ -0,0 +1,10 @@
+package grade2;
+
+public interface GradeService { //메소드 정의 추상적으로
+	public String gradeCheck(int kor,int eng, int math);
+	public void gradeInit(String name, int kor, int eng, int math);
+	public int gradeTotal();
+	public int gradeAvg();
+	public String gradeRank();
+	public String gradeResult(String result);
+}
