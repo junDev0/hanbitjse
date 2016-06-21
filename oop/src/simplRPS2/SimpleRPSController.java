afce200cf12612aba6edb81d7bd91cf8d8da25e4
hb2020 <hb2020@hb2020-PC>
2016-06-21 11:53:49 +0900

    11:53

--- oop/src/simplRPS2/SimpleRPSController.java
+++ oop/src/simplRPS2/SimpleRPSController.java
@@ -10,14 +10,13 @@ import javax.swing.JOptionPane;
  */
 public class SimpleRPSController {
 	public static void main(String[] args) {
-		String match = "",result = ""; // 결과
 		SimpleRPSService service = new SimpleRPSServiceImpl();
 		while (true) {
 			switch (JOptionPane.showInputDialog("===가위바위보 게임===\nGO(1) or STOP(0)")) {
 			case "1":				
 				JOptionPane.showMessageDialog(null,service.getResult(service.playgame(JOptionPane.showInputDialog("1.가위 2.바위 3.보"))));				
 				break;
-			case "0": result = "CLOSE"; return;	
+			case "0": JOptionPane.showMessageDialog(null, "CLOSE"); return;	
 
 			default:
 				break;
