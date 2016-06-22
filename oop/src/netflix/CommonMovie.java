a2fcbfe509187116f40069e68b58ac96411980a4
hb2020 <hb2020@hb2020-PC>
2016-06-22 19:36:04 +0900

    19:35

--- oop/src/netflix/CommonMovie.java
+++ oop/src/netflix/CommonMovie.java
@@ -1,6 +1,10 @@
 package netflix;
 
 public class CommonMovie {
-	// 비슷한 동영상 리스트
-	String MovieList[];
+	// 비슷한 동영상 리스트 , 제작년도, 연령제한, 상영시간,영화줄거리
+	private String MovieList[],createYear[],limitAge[],runningTime[],movieStory[];
+	
+	//영화별점
+	private int movieScore[];
+	
 }
--- oop/src/netflix/NetflixMain.java
+++ oop/src/netflix/NetflixMain.java
@@ -2,9 +2,9 @@ package netflix;
 
 public class NetflixMain { //Netflix 메인페이지.
 	// 내동영상목록,넷플릭스 오리지널 목록, 인기영화목록,현재영화목록
-	String myMovieList[],netflixOriginalList[],moviePopList[],nowMovieList[];
+	private String myMovieList[],netflixOriginalList[],moviePopList[],nowMovieList[];
 	// 검색단어, 
-	String serchWord;
+	private String serchWord;
 	
 	
 }
